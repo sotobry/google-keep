c54d085b8c258f3d26c62390479b52731f1cf4d6
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-01-11 07:25:26 -0800

    Finishes lesson Google Keep 8.

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ class App {
       this.handleFormClick(event);
       this.selectNote(event);
       this.openModal(event);
+      this.deleteNote(event);
     });
 
     document.body.addEventListener('mouseover', event => this.openTooltip(event));
@@ -97,6 +98,7 @@ class App {
   }
 
   openModal(event) {
+    if (event.target.matches('.toolbar-delete')) return;
     if (event.target.closest(".note")) {
       this.$modal.classList.toggle("open-modal");
       this.$modalTitle.value = this.title;
@@ -164,6 +166,14 @@ class App {
     this.id = $selectedNote.dataset.id;
   }
 
+  deleteNote(event) {
+    event.stopPropagation();
+    if (!event.target.matches('.toolbar-delete')) return;
+    const id = event.target.dataset.id;
+    this.notes = this.notes.filter(note => note.id !== Number(id));
+    this.displayNotes();
+  }
+
   displayNotes() {
     const hasNotes = this.notes.length > 0;
 
@@ -177,13 +187,12 @@ class App {
         <div class="toolbar-container">
           <div class="toolbar">
             <img src="https://icon.now.sh/palette" alt="" class="toolbar-color" data-id='${note.id}'>
-            <img src="https://icon.now.sh/delete" alt="" class="toolbar-delete">
-          </div>
-        </div>
-      </div>
-    `).join("");
+            <img src="https://icon.now.sh/delete" alt="" class="toolbar-delete" data-id='${note.id}'>
+          </div >
+        </div >
+      </div >
+  `).join("");
 
   }
 }
-
 new App();
